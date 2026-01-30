b2d539b90880bd30276ebc2b55937d12c813a6c6
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2026-01-30 10:04:49 +0100

    Update

--- src/projection.ts
+++ src/projection.ts
@@ -7,67 +7,69 @@ import { settings } from "./settings";
  * @returns An array containing the initialized data points.
  */
 function initializeDataStructure(): PlotData[] {
-    settings.livingCosts = settings.netSalary - (12 * settings.monthlySavings);
+  settings.livingCosts = settings.netSalary - 12 * settings.monthlySavings;
 
-    const data: PlotData[] = Array(settings.lifeExpectancy - settings.currentAge + 1)
-        .fill(0)
-        .map((_, index: number) => ({
-            // Initialize values that do not depend on others
-            year: index,
-            age: settings.currentAge + index,
-            inflation: Math.pow(1 + settings.averageInflation, index)
-        }))
-        .map((value: { year: number; age: number; inflation: number }) => ({
-            // Use previous values to initialize income and the annual increase.
-            // NOTE: the incomeIncrease is relative to the initial income
-            ...value,
-            income:
-                value.age < settings.retirementAge
-                    ? settings.netSalary
-                    : settings.retirementIncome,
-            incomeIncrease:
-                value.age < settings.retirementAge
-                    ? Math.pow(1 + settings.salaryIncrease, value.year)
-                    : Math.pow(
-                          1 + settings.retirementIncomeIncrease,
-                          value.age - settings.retirementAge
-                      )
-        }))
-        .map(
-            (value: {
-                year: number;
-                age: number;
-                inflation: number;
-                income: number;
-                incomeIncrease: number;
-            }) => ({
-                // Calculate the living costs and annual income with taking
-                // the yearly increase into account.
-                ...value,
-                income: value.income * value.incomeIncrease,
-                livingCosts: settings.livingCosts * value.inflation
-            })
-        )
-        .map(
-            (value: {
-                year: number;
-                age: number;
-                inflation: number;
-                income: number;
-                incomeIncrease: number;
-                livingCosts: number;
-            }) => ({
-                // Calculate the savings for each year.
-                ...value,
-                savings: value.income - value.livingCosts,
-                investments: 0,
-                investedAmount: 0
-            })
-        );
-    data[0].investments += settings.currentSavings;
-    data[0].investedAmount += settings.currentSavings;
-    console.log("Initialized data:", data);
-    return data;
+  const data: PlotData[] = Array(
+    settings.lifeExpectancy - settings.currentAge + 1,
+  )
+    .fill(0)
+    .map((_, index: number) => ({
+      // Initialize values that do not depend on others
+      year: index,
+      age: settings.currentAge + index,
+      inflation: Math.pow(1 + settings.averageInflation, index),
+    }))
+    .map((value) => ({
+      // Use previous values to initialize income and the annual increase.
+      // NOTE: the incomeIncrease is relative to the initial income
+      ...value,
+      income:
+        value.age < settings.retirementAge
+          ? settings.netSalary
+          : settings.retirementIncome,
+      incomeIncrease:
+        value.age < settings.retirementAge
+          ? Math.pow(1 + settings.salaryIncrease, value.year)
+          : Math.pow(
+              1 + settings.retirementIncomeIncrease,
+              value.age - settings.retirementAge,
+            ),
+    }))
+    .map(
+      (value: {
+        year: number;
+        age: number;
+        inflation: number;
+        income: number;
+        incomeIncrease: number;
+      }) => ({
+        // Calculate the living costs and annual income with taking
+        // the yearly increase into account.
+        ...value,
+        income: value.income * value.incomeIncrease,
+        livingCosts: settings.livingCosts * value.inflation,
+      }),
+    )
+    .map(
+      (value: {
+        year: number;
+        age: number;
+        inflation: number;
+        income: number;
+        incomeIncrease: number;
+        livingCosts: number;
+      }) => ({
+        // Calculate the savings for each year.
+        ...value,
+        savings: value.income - value.livingCosts,
+        investments: 0,
+        investedAmount: 0,
+      }),
+    );
+  data[0].investments += settings.currentSavings;
+  data[0].investedAmount += settings.currentSavings;
+  console.log("Initialized data:", data);
+  return data;
 }
 
 /**
@@ -78,9 +80,13 @@ function initializeDataStructure(): PlotData[] {
  * @param tax Capital gains tax when selling asset.
  * @returns The gross value of the asset.
  */
-function calculateGrossValue(netValue: number, growthFactor: number, tax: number) {
-    const profitMargin = (growthFactor - 1) / growthFactor;
-    return netValue / (1 - profitMargin * tax);
+function calculateGrossValue(
+  netValue: number,
+  growthFactor: number,
+  tax: number,
+) {
+  const profitMargin = (growthFactor - 1) / growthFactor;
+  return netValue / (1 - profitMargin * tax);
 }
 
 /**
@@ -90,33 +96,33 @@ function calculateGrossValue(netValue: number, growthFactor: number, tax: number
  *                 calculations will be based.
  */
 export function createProjection() {
-    const projections = initializeDataStructure();
+  const projections = initializeDataStructure();
 
-    for (let i = 1; i < projections.length; i++) {
-        const previous = projections[i - 1];
-        const current = projections[i];
+  for (let i = 1; i < projections.length; i++) {
+    const previous = projections[i - 1];
+    const current = projections[i];
 
-        if (current.savings >= 0) {
-            current.investedAmount = previous.investedAmount + previous.savings;
-            current.investments =
-                previous.investments * (1 + settings.rateOfReturn) + previous.savings;
-        } else if (previous.investments > 0) {
-            const deficit = Math.abs(current.savings);
-            const growth = previous.investments / previous.investedAmount;
-            const toSell = calculateGrossValue(deficit, growth, settings.tax);
-            // console.log(`Selling ${toSell}. Net ${deficit}`);
+    if (current.savings >= 0) {
+      current.investedAmount = previous.investedAmount + previous.savings;
+      current.investments =
+        previous.investments * (1 + settings.rateOfReturn) + previous.savings;
+    } else if (previous.investments > 0) {
+      const deficit = Math.abs(current.savings);
+      const growth = previous.investments / previous.investedAmount;
+      const toSell = calculateGrossValue(deficit, growth, settings.tax);
+      // console.log(`Selling ${toSell}. Net ${deficit}`);
 
-            current.investments = Math.max(
-                0,
-                (previous.investments - toSell) * (1 + settings.rateOfReturn)
-            );
-            current.investedAmount = Math.max(
-                0,
-                previous.investedAmount - toSell / growth
-            );
-        }
+      current.investments = Math.max(
+        0,
+        (previous.investments - toSell) * (1 + settings.rateOfReturn),
+      );
+      current.investedAmount = Math.max(
+        0,
+        previous.investedAmount - toSell / growth,
+      );
     }
-    console.log("Created projections:", projections);
+  }
+  console.log("Created projections:", projections);
 
-    return projections;
+  return projections;
 }
--- src/render.ts
+++ src/render.ts
@@ -4,36 +4,36 @@ import { setupEventListeners } from "./setup";
 import { PlotData } from "./IPlotData";
 
 function renderControls() {
-    let footnoteCounter = 1;
-    const settingsMap = new Map(Object.entries(settings));
+  let footnoteCounter = 1;
+  const settingsMap = new Map(Object.entries(settings));
 
-    const inputElements = inputs
-        .map((input) => {
-            const {
-                label,
-                description,
-                errorMessage,
-                settingsKey,
-                isPercentage,
-                id,
-                footnote,
-                min,
-                max,
-                steps
-            } = input;
-            const value = settingsMap.get(settingsKey)!;
+  const inputElements = inputs
+    .map((input) => {
+      const {
+        label,
+        description,
+        errorMessage,
+        settingsKey,
+        isPercentage,
+        id,
+        footnote,
+        min,
+        max,
+        steps,
+      } = input;
+      const value = settingsMap.get(settingsKey)!;
 
-            const footnoteLink = footnote
-                ? `<a href="#${id}-footnote">[${footnoteCounter++}]</a>`
-                : "";
-            const helpText = `<p id="${id}-help" class="help">
+      const footnoteLink = footnote
+        ? `<a href="#${id}-footnote">[${footnoteCounter++}]</a>`
+        : "";
+      const helpText = `<p id="${id}-help" class="help">
                                 ${description} ${footnoteLink}
                               </p>`;
-            const errorText = `<p id="${id}-error" class="help is-danger is-hidden">
+      const errorText = `<p id="${id}-error" class="help is-danger is-hidden">
                                 ${errorMessage ? errorMessage : ""}
                               </p>`;
 
-            return `<div class="cell">
+      return `<div class="cell">
                         <div class="field">
                             <label class="label" for="${id}">${label}</label>
                             <div class="field has-addons mb-0">
@@ -49,53 +49,52 @@ function renderControls() {
                                         max="${max}"
                                     />
                                 </div>
-                                ${
-                                    input.addon ? input.addon() : ""
-                                }
+                                ${input.addon ? input.addon() : ""}
                             </div>
                             ${helpText}
                             ${errorText}
                         </div>
                     </div>`;
-        })
-        .join("");
+    })
+    .join("");
 
-    const resetButton = `<button id="reset" class="button is-ghost has-text-grey-light m-auto">
+  const resetButton = `<button id="reset" class="button is-ghost has-text-grey-light m-auto">
                              Reset values
                          </button>`;
 
-    document.getElementById("controls")!.innerHTML = inputElements + resetButton;
+  document.getElementById("controls")!.innerHTML = inputElements + resetButton;
 
-    setupEventListeners();
+  setupEventListeners();
 }
 
 function renderFootnotes() {
-    const footnotes = inputs
-        .filter((input) => input.footnote)
-        .map((input, index) => {
-            const { id, footnote } = input;
-            return `<p id="${id}-footnote" class="has-text-grey">
+  const footnotes = inputs
+    .filter((input) => input.footnote)
+    .map((input, index) => {
+      const { id, footnote } = input;
+      return `<p id="${id}-footnote" class="has-text-grey">
                         [${index + 1}] ${footnote}
                     </p>`;
-        })
-        .join("");
-    document.getElementById("footnotes")!.innerHTML = footnotes;
+    })
+    .join("");
+  document.getElementById("footnotes")!.innerHTML = footnotes;
 }
 
 function renderTable(data: PlotData[]) {
-    const rows = data
-        .map(
-            (row: PlotData) =>
-                `<tr>
+  const rows = data
+    .map(
+      (row: PlotData) =>
+        `<tr>
                     <td>${row.age}</td>
+                    <td>${Math.round(row.investedAmount)}</td>
                     <td>${Math.round(row.investments)}</td>
                     <td>${Math.round(row.savings)}</td>
                     <td>${Math.round(row.income)}</td>
                     <td>${Math.round(row.livingCosts)}</td>
-                </tr>`
-        )
-        .join("");
-    document.getElementById("data-table")!.innerHTML = rows;
+                </tr>`,
+    )
+    .join("");
+  document.getElementById("data-table")!.innerHTML = rows;
 }
 
 export { renderControls, renderFootnotes, renderTable };
